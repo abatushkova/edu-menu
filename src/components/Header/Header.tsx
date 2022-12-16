6321105301d2e6a79551f7cf8350dcbe72388743
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-16 21:24:55 +0300

    add class names

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <header className="py-6 md:py-8 px-4 text-center">
-      <h1 className="text-3xl md:text-5xl font-bold">Cafe Green</h1>
+    <header className="py-10 md:py-12 text-center">
+      <h1 className="text-4xl md:text-5xl font-bold">Cafe Green</h1>
     </header>
   );
 }
