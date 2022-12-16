66377a93c53b0dcf2b9c489bb12ff78d58d7898d
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-16 21:25:38 +0300

    add filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
-export const Filter = () => {
+interface IFfilterModel {
+  categories: string[],
+  filterCards: (p: string) => void,
+}
+
+
+export const Filter = (props: IFfilterModel) => {
+  const { categories, filterCards } = props;
+
   return (
-    <div className="">
+    <div className="flex flex-col md:flex-row items-center justify-center gap-3 md:gap-6 pb-10 md:pb-12">
+      {categories.map((category, index) => (
+        <button
+          type="button"
+          key={index}
+          className="
+            text-xl font-bold uppercase
+            border-b-4 border-b-white
+            hover:border-b-orange-400
+            transition-colors
+          "
+          onClick={() => filterCards(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
